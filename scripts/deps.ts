d4532bfb0c48e49368a60244bb1573c1506cb592
Bitcoin Fat Cat <94967939+ABlanford@users.noreply.github.com>
2025-12-06 20:00:50 -0600

    Update deps.ts for 0.3.5.x SDK

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,4 +1,1 @@
-// Start9 SDK Dependencies
-// These are provided by the StartOS runtime
-
-export { types, compat } from "https://deno.land/x/embassyd_sdk@v0.3.4.0.2/mod.ts";
+export { types, compat, util } from "https://deno.land/x/embassyd_sdk@v0.3.5/mod.ts";
