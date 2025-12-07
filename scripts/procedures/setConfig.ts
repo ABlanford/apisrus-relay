8336279eb240910fdbfdcd4727b0ef421d1d54e9
Bitcoin Fat Cat <94967939+ABlanford@users.noreply.github.com>
2025-12-07 00:34:00 -0600

    Update setConfig.ts

--- scripts/procedures/setConfig.ts
+++ scripts/procedures/setConfig.ts
@@ -2,11 +2,11 @@ import { types as T } from "../deps.ts";
 
 export const setConfig: T.ExpectedExports.setConfig = async (
   _effects: T.Effects,
-  input: T.Config,
+  _input: T.Config,
 ) => {
   return {
     result: {
-      signal: "SIGTERM" as const,
+      signal: "SIGTERM",
       "depends-on": {},
     },
   };
