7bcc54d839e14a5c170b7664a6acaa87015cebb8
Bitcoin Fat Cat <94967939+ABlanford@users.noreply.github.com>
2025-12-07 00:50:51 -0600

    Update embassy.ts

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,2 +1,16 @@
+import { types as T } from "./deps.ts";
+
 export { migration } from "./procedures/migrations.ts";
 export { health } from "./procedures/healthChecks.ts";
+
+export const setConfig: T.ExpectedExports.setConfig = async () => {
+  return { result: { signal: "SIGTERM" as const, "depends-on": {} } };
+};
+
+export const getConfig: T.ExpectedExports.getConfig = async () => {
+  return { result: { spec: {}, config: {} } };
+};
+
+export const properties: T.ExpectedExports.properties = async () => {
+  return { result: { version: 2, data: {} } };
+};
